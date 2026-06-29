233c153d65cf4c0db6377831393289da775a6e6c
Niranjana Devi N <152769969+Nira23cb@users.noreply.github.com>
2026-06-29 13:31:46 +0530

    Spring Core Maven

---
+++ Spring Maven/EX 1,2,4/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,17 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void serviceMethod() {
+        System.out.println("Book Service Method Called");
+        bookRepository.display();
+    }
+}
