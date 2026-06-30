368cfff4d1df6690a4662d4e44eb7f921b50068f
Niranjana Devi N <152769969+Nira23cb@users.noreply.github.com>
2026-06-30 13:23:05 +0530

    Spring Data JPA hands-on

---
+++ Spring Data JPA/EX 1/src/main/java/orm_learn/service/CountryService.java
@@ -0,0 +1,19 @@
+package orm_learn.service;
+
+import orm_learn.model.Country;
+import orm_learn.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+}
